c101cdfd2ad81deee4f1497223bcbb9654e68487
Mr. Lo <55058939+mrlo-codes@users.noreply.github.com>
2023-06-02 13:06:20 -0400

    Updated search capabilities

--- weather.js
+++ weather.js
@@ -1,16 +1,20 @@
 let weather = {
     "apiKey": "b1e150d000289baf9d30f53147f908b2",
+
     fetchWeather: function(city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
         ).then((response) => response.json())
         .then((data) => this.displayWeather(data));
+    },
+    fetchCSVWeather: function(kjgh){
+        
     },
     displayWeather: function(data){
         const {name} = data;
         const {icon, description} = data.weather[0];
         const {temp, humidity} = data.main;
         const {speed} = data.wind;
-        console.log(name, icon, description, temp, humidity, speed);
+        //console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -25,8 +29,15 @@ let weather = {
 };
 
 document.querySelector(".search button").addEventListener("click", function(){
-    weather.search();
+    setInterval(weather.search(), 10000);
 });
+
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
 
 
    
